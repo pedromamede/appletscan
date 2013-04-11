1c0634a020ce1151c6982705886160a4c408a8a4
Pedro Mamede <pedro.mamede@gmail.com>
2013-04-11 13:50:41 -0300

    Retirando debug do for no upload de imagens

--- AppletScan/src/br/com/miltecnologia/main/Digitalizar.java
+++ AppletScan/src/br/com/miltecnologia/main/Digitalizar.java
@@ -176,14 +176,10 @@ public class Digitalizar extends javax.swing.JApplet {
         }else{
             ArrayList listaImagens = scanner.getListaImagens();
             JOptionPane.showMessageDialog(this,listaImagens.size()+" imagens foram digitalizadas.");
-            int i = 1;
             for (Object imagem: listaImagens) {
                 String imgString = scanner.encodeToString((BufferedImage)imagem, "jpeg");
                 enviarImagemViaJs(imgString);
-                JOptionPane.showMessageDialog(this,i+" imagem enviada.");
-                i++;
             }
-            i = 0;
         }
     }//GEN-LAST:event_jbUploadActionPerformed
 
