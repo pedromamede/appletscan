07f25f156a95f2e8fd7abb7e0bb3d32b4db3351b
pedromamede <pedro.mamede@gmail.com>
2013-04-11 12:12:46 -0300

    Adicionando numero random ao nome do arquivo upload

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,7 +4,7 @@ class ImagesController < ApplicationController
 
   	image_decoded = image_data.tr('-_','+/').unpack('m')[0]
 
-  	File.open("#{Rails.root.to_s}/public/tmp/#{Time.now.to_i}_imagem.jpeg",'wb') do |f|
+  	File.open("#{Rails.root.to_s}/public/tmp/#{Time.now.to_i}_#{rand(1..10000}_imagem.jpeg",'wb') do |f|
 			f.write image_decoded
 		end
   end
